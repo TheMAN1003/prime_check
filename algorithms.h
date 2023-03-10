8cb41ffe3160ab182a1699b3ad96f54add5cd59c
akgzulag <burzhymskyi.rostyslav@gmail.com>
2023-03-11 00:52:20 +0200

    init project

---
+++ algorithms.h
@@ -0,0 +1,16 @@
+//
+// Created by Akgzulag on 11.03.2023.
+//
+
+#ifndef NTA_LAB_1_ALGORITHMS_H
+#define NTA_LAB_1_ALGORITHMS_H
+
+namespace algo {
+
+    class algorithms {
+
+    };
+
+} // algo
+
+#endif //NTA_LAB_1_ALGORITHMS_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Akgzulag on 11.03.2023.
+//
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
