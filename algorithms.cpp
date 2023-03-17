a3fe129c82612abc91e24495c176d9540db45864
akgzulag <burzhymskyi.rostyslav@gmail.com>
2023-03-17 23:34:28 +0200

    added functions creation of continued fraction, buildeing base, jacobi, factorisation b2

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,29 +1,173 @@
 #include "algorithms.h"
 
-ll algo::count(ll n) {
-    int count = 0;
-    while(n != 0){
-        n=n/10;
-        count++;
+namespace algo
+{
+    namespace
+    {
+        ll count(ll n)
+        {
+            int count = 0;
+            while(n != 0)
+            {
+                n=n/10;
+                count++;
+            }
+            return count;
+        }
+
+        ll digit(ll a, int position)
+        {
+            for (int i = 0; i < position; i++){
+                a=a/10;
+            }
+            a=a%10;
+            return a;
+        }
+
+        ll find_r(ll mod, ll position)
+        {
+            ll r = 1;
+            for(int i=0; i < position; i++)
+            {
+                r = (r*10)%mod;
+            }
+            return r;
+        }
+        ll function(long long x, ll mod)
+        {
+            return (x*x+1)%mod;
+        }
+
+        ll gcd(long long a, long long b)
+        {
+            ll d = 1;
+            while (d!=0)
+            {
+                d=a%b;
+                a=b;
+                b=d;
+            }
+            return a;
+        }
+
+        ll powerMod(ll x, ll pow,ll mod)
+        {
+            ll C = 1;
+            for(int i = int(log2(pow)); i >= 0; --i)
+            {
+                if((pow>>i & 1) == 1)
+                {
+                    C = (C * x)%mod;
+                }
+                if(i != 0)
+                    C = (C * C)%mod;
+            }
+            return C;
+        }
+
+        int jacobi(ll x, ll n)
+        {
+            int t = 1;
+            while (x != 0) {
+                while (x % 2 == 0) {
+                    x /= 2;
+                    int r = n % 8;
+                    if (r == 3 || r == 5) t = -t;
+                }
+                int temp = x;
+                x = n;
+                n = temp;
+                if (x % 4 == 3 && n % 4 == 3) t = -t;
+                x %= n;
+            }
+            if (n == 1) return t;
+            else return 0;
+        }
+
+        std::vector<ll> buildBase(const ll& n, std::vector<ll>& primes, const double& alpha)
+        {
+            long long L = pow(exp(sqrt(log(n)*log(log(n)))), alpha);
+
+            std::vector<ll> base;
+            base.push_back(-1);
+            for(auto p : primes)
+            {
+                if(jacobi(n,p) == 1)
+                    base.push_back(p);
+            }
+
+            return base;
+        }
+
+        std::pair<std::vector<ll>,std::vector<ll>> continuedFraction(ll n,int k)
+        {
+            ll v[2],a[2],u[2];
+            v[0] = 1;
+            a[0] = (ll)sqrt(n);
+            u[0] = a[0];
+            double alpha,sqrtN = sqrt(n);
+            std::vector<ll> b(k);
+            std::vector<ll> b2(k);
+            b[0] = a[0];
+            b2[0] = (b[0]*b[0])%n;
+            if(b2[0] > n - b2[0])
+                b2[0] = n- b2[0];
+            for(int i = 1; i < k; ++i)
+            {
+                v[i % 2] = (n - u[(i-1) % 2 ] * u[(i - 1) % 2]);
+                alpha = (sqrtN + u[(i - 1) % 2])/v[i % 2];
+                a[i % 2] = (ll)alpha;
+                u[i % 2] = a[i % 2] * v[i % 2] - u[(i - 1) % 2];
+                if(i == 1)
+                {
+                    b[i] = a[i]*b[i-1] + 1;
+                } else
+                {
+                    b[i] = a[i]*b[i-1] + b[i-2];
+                }
+                b[i] = (b[i]*b[i]) % n;
+                b2[i] = (b[i]*b[i])%n;
+                if(b2[i] > n - b2[i])
+                    b2[i] = n- b2[i];
+            }
+            return std::pair<std::vector<ll>,std::vector<ll>>(b,b2);
+        }
+
+        std::vector<ll> factorB(ll x, const std::vector<ll>& base)
+        {
+            std::vector v(base.size(),0);
+            while (x != 1)
+            {
+                if(x < 0)
+                {
+                    v[0] = 1;
+                    x *= -1;
+                }
+                for(int i = 1; i < base.size(); ++i)
+                {
+                    int k = 0;
+                    while(x % base[i] == 0)
+                    {
+                        x/=base[i];
+                        ++k;
+                    }
+                    v[i] = k%2;
+                }
+            }
+        }
+
+        std::vector<std::vector<ll>> createSystem(const std::vector<ll> b2,const std::vector<ll>& base)
+        {
+            std::vector<std::vector<ll>> sys;
+            for(auto i: b2)
+            {
+                sys.push_back(factorB(i,base));
+            }
+        }
+
     }
-    return count;
 }
 
-ll algo::digit(ll a, int position) {
-    for (int i = 0; i < position; i++){
-        a=a/10;
-    }
-    a=a%10;
-    return a;
-}
-
-ll algo::find_r(ll mod, ll position){
-    int r = 1;
-    for(int i=0; i < position; i++){
-        r = (r*10)%mod;
-    }
-    return r;
-}
 
 ll algo::trial(long long n) {
     for (int j=2; j < 48; j++) {
@@ -39,40 +183,11 @@ ll algo::trial(long long n) {
     return -1;
 }
 
-ll algo::function(long long x, ll mod) {
-    return (x*x+1)%mod;
-}
-
-ll algo::gcd(long long a, long long b) {
-    ll d = 1;
-    while (d!=0){
-        d=a%b;
-        a=b;
-        b=d;
-    }
-    return a;
-}
-
-ll algo::powerMod(ll x, ll pow,ll mod)
-{
-    ll C = 1;
-    for(int i = int(log2(pow)); i >= 0; --i)
-    {
-        if((pow>>i & 1) == 1)
-        {
-            C = (C * x)%mod;
-        }
-        if(i != 0)
-            C = (C * C)%mod;
-    }
-    return C;
-}
-
 ll algo::pollard(long long n) {
     ll x = 1;
     ll y = 1;
     ll d = 1;
-    while(d==1){
+    while(d == 1){
         x = function(x, n);
         y = function(function(y, n), n);
         d = gcd(abs(x-y), n);
@@ -114,6 +229,20 @@ bool algo::MillerRabin(long long p)
         ++j;
     }
     return 0;
+}
+
+ll algo::methodBrillhartMorrison(ll n, std::vector<ll> primes)
+{
+    double alpha = 1/sqrt(2);
+
+    std::vector base = buildBase(n,primes,alpha);
+    std::vector<ll> b,b2;
+
+    auto t = continuedFraction(n,primes.size());
+    b = t.first;
+    b2 = t.second;
+
+    //b = b + 1/(4*b);
 }
 
 
--- algorithms.h
+++ algorithms.h
@@ -4,21 +4,16 @@
 #define ll long long
 
 #include <iostream>
-#include <ctime>
 #include <cmath>
 #include <cstdlib>
 #include <vector>
 
-namespace algo {
-    ll count(ll n);
-    ll digit(ll n, int position);
-    ll find_r(ll mod, ll number);
+namespace algo
+{
     ll trial(ll n);
-    ll function(ll x, ll mod);
-    ll gcd(ll a, ll b);
     ll pollard(ll n);
     bool MillerRabin(ll p);
-    ll powerMod(ll x, ll pow,ll mod);
-} // algo
+    ll methodBrillhartMorrison(ll n,std::vector<ll> primes);
+}
 
 #endif //NTA_LAB_1_ALGORITHMS_H
